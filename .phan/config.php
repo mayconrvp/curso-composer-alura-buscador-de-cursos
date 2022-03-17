167510f3405090874c04bdc1d7be7e07bb392ac1
Maycon <maycon.rvp@outlook.com>
2022-03-17 10:52:31 -0300

    Primeiro commit

---
+++ .phan/config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This configuration will be read and overlaid on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ */
+return [
+
+    
+    "target_php_version" => null,
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle'
+    ],
+
+    
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+
+    'plugins' => [
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+        'SleepCheckerPlugin',
+        'UnreachableCodePlugin',
+        'UseReturnValuePlugin',
+        'EmptyStatementListPlugin',
+        'LoopVariableReusePlugin',
+    ],
+];
---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php 
+
+function exibeMensagem(string $msg){
+  echo $msg . PHP_EOL;
+}
